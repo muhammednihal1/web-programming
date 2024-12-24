c7e5f50b03b1016667e7f0bc369bb847f93c6a30
muhammednihal1 <muhammednihall@proton.me>
2024-12-24 21:30:01 +0530

    Add files via upload

---
+++ experiment 14.php
@@ -0,0 +1,11 @@
+<?php
+$name = ['jaseem','nabeel','nihal','roshan','linto'];
+echo "Displaying using print_r <br>";
+print_r($name);
+echo "<br> Sorting using asort <br>";
+asort($name);
+print_r($name);
+echo "<br> Sorting using asort <br>";
+arsort($name);
+print_r($name);
+?>
